c28c5f53b78e23f117b915ae60ac4b967b149cbd
hanaafriliza1012 <48717980+hanaafriliza1012@users.noreply.github.com>
2019-04-24 22:51:50 +0700

    Update Using a Stack to Evaluate an Expression.cpp

--- Using a Stack to Evaluate an Expression.cpp
+++ Using a Stack to Evaluate an Expression.cpp
@@ -1,1 +1,16 @@
 #include<iostream>
+using namespace std;
+int main(){
+  class node
+{
+ public:
+    int data;
+    node* next;
+};
+  int priority(char a) {
+    int temp;
+    if (a == '^')
+        temp = 1;
+    else if (a == '*' || a == '/')
+        temp = 2;
+    else if (a == '+' || a == '-')
